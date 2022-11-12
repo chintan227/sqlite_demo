88455df2c40212ec413c1cce1042b17fb5acbd92
Chintan mac <dreamworld@DREAMWORLDs-iMac-Pro-3.local>
2022-11-12 15:57:06 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/sqlite_database_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sqlite_database_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/sqlite_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SqliteHomePage extends StatefulWidget {
+  const SqliteHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<SqliteHomePage> createState() => _SqliteHomePageState();
+}
+
+class _SqliteHomePageState extends State<SqliteHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
